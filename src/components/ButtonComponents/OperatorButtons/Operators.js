f3b3abba8151839248b5d2ab2e46cc2344bea7bc
tlaudahl <travis.laudahl@gmail.com>
2019-09-10 17:49:15 -0500

    Finished MVP and Stretch - Designed whole calculator to look like design file

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { operators } from '../../../data';
+import OperatorButton from './OperatorButton';
 
 //import any components needed
 
@@ -11,6 +13,11 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operators.map(item => (
+         <OperatorButton text={item.char}/>
+       ))}
     </div>
   );
 };
+
+export default Operators;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { specials } from '../../../data'
+import SpecialButton from './SpecialButton';
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -8,10 +9,15 @@ const Specials = () => {
   // STEP 2 - add the imported data to state
 
   return (
-    <div>
+    <div className='specialsContainer'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specials.map(item => (
+         <SpecialButton text={item}/>
+       ))}
     </div>
   );
 };
+
+export default Specials;
